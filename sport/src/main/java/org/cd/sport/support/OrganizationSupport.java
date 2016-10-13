0c936eeef303c73a1064703129b6e54ddc0a3bcd
lyk <liuyk@sysware.com.cn>
2016-10-14 00:10:46 +0800

    user和organization部分属性非空校验

--- sport/src/main/java/org/cd/sport/support/OrganizationSupport.java
+++ sport/src/main/java/org/cd/sport/support/OrganizationSupport.java
@@ -1,6 +1,8 @@
 package org.cd.sport.support;
 
+import org.apache.commons.lang.StringUtils;
 import org.cd.sport.domain.OrganizationDomain;
+import org.cd.sport.exception.ParameterIsWrongException;
 import org.cd.sport.view.OrganizationView;
 
 /**
@@ -16,7 +18,46 @@ public class OrganizationSupport extends SportSupport {
 	 * 验证用户信息合法性
 	 */
 	public void validate(OrganizationView dic) {
-
+		if(dic==null){
+			throw new ParameterIsWrongException("组织不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgFullName())){
+			throw new ParameterIsWrongException("单位全称不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgAddress())){
+			throw new ParameterIsWrongException("单位地址不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgLegalLeader())){
+			throw new ParameterIsWrongException("法人代表不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgRegion())){
+			throw new ParameterIsWrongException("所在区域不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgTelphone())){
+			throw new ParameterIsWrongException("单位电话不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgFax())){
+			throw new ParameterIsWrongException("单位传真不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgQuality())){
+			throw new ParameterIsWrongException("单位性质不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgEmail())){
+			throw new ParameterIsWrongException("组织电子信箱不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgCode())){
+			throw new ParameterIsWrongException("组织机构代码不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgPost())){
+			throw new ParameterIsWrongException("组织邮政编码不能为空");
+		}
+		if(StringUtils.isBlank(dic.getManagerName())){
+			throw new ParameterIsWrongException("业务负责人姓名不能为空");
+		}
+		if(StringUtils.isBlank(dic.getOrgTelphone())){
+			throw new ParameterIsWrongException("业务负责人不能为空");
+		}
+		
 	}
 
 	public OrganizationDomain process(OrganizationView dic) {
--- sport/src/main/java/org/cd/sport/support/UserSupport.java
+++ sport/src/main/java/org/cd/sport/support/UserSupport.java
@@ -24,11 +24,52 @@ public class UserSupport extends SportSupport {
 		}
 
 		if (StringUtils.isBlank(user.getLoginName()) || user.getLoginName().length() < 4 || user.getLoginName().length() > 20) {
-			throw new ParameterIsWrongException("用户名为空");
+			throw new ParameterIsWrongException("用户名为空或者格式不对");
 		}
+		
 
+		//校验password 
+		if(StringUtils.isBlank(user.getPassword())|| user.getPassword().length()< 6 ||user.getPassword().length()>16){
+			throw new ParameterIsWrongException("密码不能为空或者长度不对");
+		}else{//校验字母数字组合
+			String password = user.getPassword();
+			String reg1 = "\\d+$";	//不能纯数字
+	        String reg2 = "[a-z]+$";	//不能纯小写字母
+	        String reg3 = "[A-Z]+$";	//不能纯大写字母
+	        String reg4 = "((?=[\\x21-\\x7e]+)[^A-Za-z0-9])+$";	//不能纯标点
+            if(password.matches(reg1)||password.matches(reg2)||password.matches(reg3)||password.matches(reg4)){//校验纯数字,纯字母等
+            	throw new ParameterIsWrongException("密码必须是数字字母组合");
+            }else{
+            	for (int i = 0; i < password.length(); i++) {
+            		char  item =  password.charAt(i);
+            		if((int)item<32||(int)item>126){
+            			throw new ParameterIsWrongException("密码必须是数字字母组合");
+            		}
+            	}
+            }
+		}
+		if (StringUtils.isBlank(user.getUserName()) || user.getUserName().length() < 4 || user.getUserName().length() > 20) {
+			throw new ParameterIsWrongException("真实姓名为空或者格式不对");
+		}
+		
+		if(user.getGender()==0){
+			throw new ParameterIsWrongException("性别不能为空");
+		}
+		
+		if(StringUtils.isBlank(user.getCredType())){
+			throw new ParameterIsWrongException("证件类型不能为空");
+		}
+		if(StringUtils.isBlank(user.getCredNo())){
+			throw new ParameterIsWrongException("证件编号不能为空");
+		}
+		if(StringUtils.isBlank(user.getRole())){
+			throw new ParameterIsWrongException("用户角色不能为空");
+		}
+		
+		
+		
 	}
-
+	
 	public UserDomain process(UserView user) {
 		this.validate(user);
 		UserDomain userDomain = this.result(UserDomain.class, user);
